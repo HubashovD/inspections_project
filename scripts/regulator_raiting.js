b768b97a55cdeea49fe0a8b1450b007aef002515
Yevheniia Drozdova <evgesha.drozdova@gmail.com>
2021-05-08 20:39:50 +0300

    правки

--- scripts/regulator_raiting.js
+++ scripts/regulator_raiting.js
@@ -1,24 +1,21 @@
-// set the dimensions and margins of the graph
-var margin = {top: 20, right: 30, bottom: 40, left: 90},
+var margin = {top: 20, right: 30, bottom: 40, left: 300},
     width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    height = 800 - margin.top - margin.bottom;
 
-// append the svg object to the body of the page
 var svg = d3.select("#my_dataviz")
-  .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// Parse the Data
-d3.csv("/data/barplot.csv", function(data) {
+d3.csv("data/barplot.csv", function(data) {
 
-  // Add X axis
+  data.forEach(function(d){ d.ide = +d.ide;});
+
   var x = d3.scaleLinear()
     .domain([0, 50000])
     .range([ 0, width]);
+
   svg.append("g")
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x))
@@ -26,23 +23,23 @@ d3.csv("/data/barplot.csv", function(data) {
       .attr("transform", "translate(-10,0)rotate(-45)")
       .style("text-anchor", "end");
 
-  // Y axis
   var y = d3.scaleBand()
     .range([ 0, height ])
-    .domain(data.map(function(d) { return d.Regulator; }))
+    .domain(data.map(function(d) { return d.regulator; }))
     .padding(.1);
+
   svg.append("g")
-    .call(d3.axisLeft(y))
+    .call(d3.axisLeft(y));
 
   //Bars
-  svg.selectAll("myRect")
+  svg.selectAll(".myRect")
     .data(data)
     .enter()
     .append("rect")
     .attr("x", x(0) )
-    .attr("y", function(d) { return y(d.Regulator); })
-    .attr("width", function(d) { return x(d.Ide); })
+    .attr("y", function(d) { return y(d.regulator); })
+    .attr("width", function(d) { return x(d.ide); })
     .attr("height", y.bandwidth() )
     .attr("fill", "#69b3a2")
 
-})
+});
